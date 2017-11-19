d9fbe2a717de8f06e5cd77ac99b386bffe7f7316
Lionel Jeanson <lionel.jeanson@int.com>
2017-11-19 11:09:54 +0100

    first working version

--- src/main.py
+++ src/main.py
@@ -1,15 +1,16 @@
+# pip install paho-mqtt pySerial
 import paho.mqtt.client as mqtt
-from lib import maestro 
+from lib import maestro
 
-MQTT_TURNOUTS_TOPIC="trains/track/turnouts/#"
+MQTT_TURNOUTS_TOPIC="/trains/track/turnout/#"
 
-SERVO_ACC=1
-SERVO_POS_CLOSED=6000
-SERVO_POS_OPENED=0
+SERVO_ACC=5
+SERVO_POS_CLOSED=7500
+SERVO_POS_THROWN=4500
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print("Connected to MQTT bus with result code "+str(rc))
     # Subscribing in on_connect() means that if we lose the connection and reconnect then subscriptions will be renewed.
     client.subscribe(MQTT_TURNOUTS_TOPIC)
 
@@ -19,20 +20,22 @@ def on_turnout_message(client, userdata, msg):
     try:
         toid = int( msg.topic[len(MQTT_TURNOUTS_TOPIC)-1::] )
         print("turnout id : "+str(toid))
-        if (msg.topic=="OPEN"):
+        if (msg.payload=="THROWN"):
             servo.setAccel(toid, SERVO_ACC)
-            servo.setTarget(toid,SERVO_POS_OPENED)
-        elif  (msg.topic=="CLOSE"):
+            servo.setTarget(toid,SERVO_POS_THROWN)
+            print("  ...throwing")
+        elif  (msg.payload=="CLOSED"):
             servo.setAccel(toid, SERVO_ACC)
             servo.setTarget(toid,SERVO_POS_CLOSED)
+            print("  ...closing")
         else:
             raise Exception('Invalid turnout command')
     except:
-        print("Invalid turnout command :") 
+        print("Invalid turnout command :")
         print("  topic :"+ str(msg.topic))
         print("  msg :"+ str(msg.payload))
         return
-    
+
 
 
 
@@ -46,6 +49,13 @@ def on_turnout_message(client, userdata, msg):
 # init maestro
 print("Openning servo connection...")
 servo = maestro.Controller()
+for i in range(0,12):
+    print("Servo "+str(i))
+    min = servo.getMin(i)
+    max = servo.getMax(i)
+    print("    limits "+str(min)+" - "+str(max))
+    pos = servo.getPosition(i)
+    print("    position "+str(pos))
 
 # init mqtt
 client = mqtt.Client()
